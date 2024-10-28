f63043a48b0ba24b1a9164fc8d21dd718c172eed
Evandro Andrade <45280444+Evandro-J-O-Andrade@users.noreply.github.com>
2024-10-28 10:25:21 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 var MenuItens = document.getElementById("MenuItens");
-var produtoImg =document.getElementById("produtoImg");
+var produtoImg = document.getElementById("produtoImg");
 var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
 MenuItens.style.maxHeight = "0px";
 
-function menucelular(){
-    if(MenuItens.style.maxHeight == "0px"){
+function menucelular() {
+    if (MenuItens.style.maxHeight === "0px") {
         MenuItens.style.maxHeight = "200px";
         MenuItens.classList.add("menu-ativo"); // Adiciona a classe quando o menu é expandido
     } else {
@@ -13,18 +14,8 @@ function menucelular(){
     }
 }
 
-produtoMiniatura[0].onclick= function(){
-    produtoImg.src = produtoMiniatura[0].src;
-}
-
-produtoMiniatura[1].onclick =function(){
-    produtoImg.src=produtoMiniatura[1].src;
-}
-
-produtoMiniatura[2].onclick =function(){
-    produtoImg.src = produtoMiniatura[2].src;
-}
-
-produtoMiniatura[3].onclick =function(){
-    produtoImg.src=produtoMiniatura[3].src;
+for (let i = 0; i < produtoMiniatura.length; i++) {
+    produtoMiniatura[i].onclick = function() {
+        produtoImg.src = produtoMiniatura[i].src;
+    }
 }
