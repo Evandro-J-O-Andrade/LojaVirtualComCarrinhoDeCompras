2ce28c96a5ec55b2f9e4c2ce7edd69310863c02e
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-11-16 07:15:52 -0300

    Incluindo tela de cadastro

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+var MenuItens = document.getElementById("MenuItens");
+
+
+MenuItens.style.maxHeight = "0px";
+
+function menucelular() {
+    if (MenuItens.style.maxHeight === "0px") {
+        MenuItens.style.maxHeight = "200px";
+        MenuItens.classList.add("menu-ativo"); // Adiciona a classe quando o menu é expandido
+    } else {
+        MenuItens.style.maxHeight = "0px";
+        MenuItens.classList.remove("menu-ativo"); // Remove a classe quando o menu é recolhido
+    }
+}
