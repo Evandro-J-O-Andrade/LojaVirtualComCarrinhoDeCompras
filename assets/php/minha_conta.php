5affe4acd75e3b9ed9d8bc37fc784cb78a8b3ca4
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-12-06 05:38:50 -0300

    teste de banco

--- assets/php/minha_conta.php
+++ assets/php/minha_conta.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
 // Inicia a sessão
-=======
-include 'conexao.php';
- // Conexão com o banco de dados
->>>>>>> 0ae2ef0e6062fdf672db551dd1495c3dbc97bc57
 session_start();
 
 // Inclui a conexão com o banco de dados e as funções
@@ -15,15 +10,11 @@ if (isset($_SESSION['usuario_id'])) {
     exit;
 }
 
-<<<<<<< HEAD
 // Verifica se o formulário de login foi enviado
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Recebe os dados do formulário
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-=======
-require 'conexao.php';
->>>>>>> 0ae2ef0e6062fdf672db551dd1495c3dbc97bc57
 
     // Verifica o login
     $usuario = verificarLogin($email, $senha);
