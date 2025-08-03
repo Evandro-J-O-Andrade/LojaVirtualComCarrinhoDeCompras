3fc5450f2c5efe50512a11c2633c95a4009339d2
Evandro Andrade <evandro_j.o.a@hotmail.com>
2025-08-03 00:47:56 -0300

    Update chatBoot.js

--- assets/js/chatBoot.js
+++ assets/js/chatBoot.js
@@ -14,7 +14,7 @@ async function sendMessage(event) {
     resetInactivityTimer();
 
     try {
-      const response = await fetch("/api/chat", {
+      const response = await fetch("https://angel-cosmeticos.onrender.com/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ mensagem: userMessage }),
